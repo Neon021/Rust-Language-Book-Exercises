2530373a9d7bd38e5f8c3407778bfd64ef3783ae
Furkan <bilalyigit594@gmail.com>
2024-08-20 17:45:06 +0300

    init

---
+++ hello_world/target/debug/deps/hello_world-65c478c245451323.d
@@ -0,0 +1,5 @@
+/home/neon/repos/rust_book/hello_world/target/debug/deps/hello_world-65c478c245451323: src/main.rs
+
+/home/neon/repos/rust_book/hello_world/target/debug/deps/hello_world-65c478c245451323.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/neon/repos/rust_book/hello_world/target/debug/hello_world: /home/neon/repos/rust_book/hello_world/src/main.rs
